bbbe505ef4db08125d28cdbe2a4dd83d88b4ba25
Alexander O'Mara <me@alexomara.com>
2014-07-30 10:28:39 -0400

    Initial setup.

---
+++ expressions-analytics.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Name: Expressions Analytics
+Description: WordPress plugin for Expressions analytics.
+Author: Expressions Team, Alexander O'Mara
+Version: 1.0
+*/
+
+//Check if inside WordPress.
+if ( ! defined( 'ABSPATH' ) ) { exit(); }
+
