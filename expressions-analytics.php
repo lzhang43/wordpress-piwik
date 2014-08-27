42abc4d9f993ca467ba7a9186557008c81789007
Alexander O'Mara <me@alexomara.com>
2014-08-27 13:07:48 -0400

    Fixed header for non-nust-use plugins.

--- expressions-analytics.php
+++ expressions-analytics.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Name: Expressions Analytics
+Plugin Name: Expressions Analytics
 Description: WordPress plugin for Expressions analytics.
 Author: Expressions Team, Alexander O'Mara
 Version: 1.0
