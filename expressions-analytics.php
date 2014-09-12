5889d22b87a760cd29489f25d75ecba22b98bba7
Michael Zhang <lzhang43@syr.edu>
2014-09-12 13:46:10 -0400

    Add Chart.js code

--- expressions-analytics.php
+++ expressions-analytics.php
@@ -1055,7 +1055,7 @@ EOS;
         wp_enqueue_script('expana_chartjs');
 		wp_enqueue_style('expana_style');
 
-		add_meta_box( 'expana_visit_length_of_visits', 'Length of Visits (d3js)', array( $this, 'callback_dashboard_length_of_visits'), $this->pagehook, 'normal', 'core' );
+		add_meta_box( 'expana_visit_length_of_visits', 'Length of Visits (Chart.js)', array( $this, 'callback_dashboard_length_of_visits'), $this->pagehook, 'normal', 'core' );
 		add_meta_box( 'expana_visit_summary', 'Visit Summary', array( $this, 'callback_dashboard_visit_summary'), $this->pagehook, 'normal', 'core' );
 		add_meta_box( 'expana_live', 'Live', array( $this, 'callback_dashboard_live'), $this->pagehook, 'normal', 'core' );
 		add_meta_box( 'expana_visitor_map', 'Visitor Map', array( $this, 'callback_dashboard_visitor_map'), $this->pagehook, 'side', 'core' );
@@ -1069,14 +1069,51 @@ EOS;
 
 	public function callback_dashboard_length_of_visits()
 	{
-		$piwik_response = $this->query_piwik_api(array(
+		$piwik_response = $this->query_piwik_api(NULL, array(
 			'token_auth'	=> $this->get_token_auth(),
 			'idSite' 		=> '111',
 			'method'		=> 'VisitorInterest.getNumberOfVisitsPerVisitDuration',
 			'period'		=> 'day',
 			'date'			=> 'today'
-			)); ?>
+			)); 
 
+			var_dump($piwik_response);
+
+			?>
+			
+		<canvas id="myChart" width="400" height="400"></canvas>
+		
+		<script language="JavaScript">
+            jQuery(document).ready(function($) {
+                $('#myChart').attr('width', $('#myChart').parent().width());
+                
+                new Chart(document.getElementById("myChart").getContext("2d")).Line({
+                    labels : ["January", "February", "March", "April", "May", "June", "July"],
+                    datasets : [
+                        {
+                            label: "My First dataset",
+                            fillColor: "rgba(220,220,220,0.2)",
+                            strokeColor: "rgba(220,220,220,1)",
+                            pointColor: "rgba(220,220,220,1)",
+                            pointStrokeColor: "#fff",
+                            pointHighlightFill: "#fff",
+                            pointHighlightStroke: "rgba(220,220,220,1)",
+                            data: [65, 59, 80, 81, 56, 55, 40]
+                        },
+                        {
+                            label: "My Second dataset",
+                            fillColor: "rgba(151,187,205,0.2)",
+                            strokeColor: "rgba(151,187,205,1)",
+                            pointColor: "rgba(151,187,205,1)",
+                            pointStrokeColor: "#fff",
+                            pointHighlightFill: "#fff",
+                            pointHighlightStroke: "rgba(151,187,205,1)",
+                            data: [28, 48, 40, 19, 86, 27, 90]
+                        }
+                    ]
+                });
+            });
+		</script>
 	<?php
 	}
 
