6ed6c9712bcb8297e3e889144b038a9d52708cfb
Michael Zhang <lzhang43@syr.edu>
2014-10-17 14:24:53 -0400

    Fix floats

--- expressions-analytics.php
+++ expressions-analytics.php
@@ -1654,14 +1654,14 @@ EOS;
 			                        borderWidth: 0,
 			                        dataLabels: {
 			                            enabled: true,
-			                            format: '{point.y:.1f}'
+			                            format: '{point.y:.0f}'
 			                        }
 			                    }
 			                },
 
 			                tooltip: {
 			                    headerFormat: '<span style="font-size:11px">{series.name}</span><br>',
-			                    pointFormat: '<span style="color:{point.color}">{point.name}</span>: <b>{point.y:.2f}</b> of total<br/>'
+			                    pointFormat: '<span style="color:{point.color}">{point.name}</span>: <b>{point.y:.0f}</b><br/>'
 			                },
 
 			                series: [{
